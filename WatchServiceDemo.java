d73e04363ec75e31ab6f4aad1154b1a99b45a600
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-13 10:45:54 +0530

    Added UC3: Watch Service

---
+++ WatchServiceDemo.java
@@ -0,0 +1,83 @@
+package employee.javaFileIO;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class WatchServiceDemo {
+    private final WatchService watcher;
+    private final Map<WatchKey,Path> dirWatchers;
+    WatchServiceDemo(Path dir) throws IOException {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.dirWatchers = new HashMap<>();
+        scanRegisterDir(dir);
+    }
+
+    private void scanRegisterDir(final Path start) throws IOException {
+        Files.walkFileTree(start, new SimpleFileVisitor<>(){
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs){
+                registerDir(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+    }
+
+    private void registerDir(Path dir) throws IOException {
+        WatchKey key=dir.register(watcher,ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY);
+        dirWatchers.put(key,dir);
+    }
+    @SuppressWarnings({"rawtypes","unchecked"})
+    public void processEvents(){
+        long count=1;
+        while (true){
+            WatchKey key;
+            try {
+                key=watcher.take();
+                count++;
+            }
+            catch (InterruptedException x){
+                System.out.println("count : "+count);
+                return;
+            }
+            Path dir=dirWatchers.get(key);
+            if(dir==null){
+                continue;
+            }
+            for (WatchEvent<?> event:key.pollEvents()){
+                WatchEvent.Kind kind=event.kind();
+                Path name=((WatchEvent<Path>)event).context();
+                Path child=dir.resolve(name);
+                System.out.println(event.kind()+" "+child);//print out the event
+                //if dir created then register it and it's sub directory
+                if(kind==ENTRY_CREATE){
+                    try {
+                        if(Files.isDirectory(child)) scanRegisterDir(child);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (kind.equals(ENTRY_DELETE)) {
+                    if (Files.isDirectory(child)) dirWatchers.remove(key);
+                }
+                System.out.println("Keep watching yes/no");
+
+                if(sc.nextLine().toLowerCase().equals("no")){
+                    System.out.println("count : "+ count);
+                    break;
+                }
+            }
+            //reset key if dir is removed
+            boolean valid=key.reset();
+            if(!valid){
+                dirWatchers.remove(key);
+                if (dirWatchers.isEmpty()){
+                    break;
+                }
+            }
+        }
+    }
+}
